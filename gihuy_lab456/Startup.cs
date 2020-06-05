770baeca00a076d93d69d7f9373a394752c105f3
nguyentrangiahuy <nguyentrangiahuya8.phuhoa1617@gmail.com>
2020-06-05 15:36:59 +0700

    Add project files.

---
+++ gihuy_lab456/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(gihuy_lab456.Startup))]
+namespace gihuy_lab456
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
