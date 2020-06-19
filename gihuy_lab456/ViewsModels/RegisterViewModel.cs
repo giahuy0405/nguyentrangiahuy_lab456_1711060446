d3c33d99701a532d92b346d2e15d5ed818ee37d6
nguyentrangiahuy <nguyentrangiahuya8.phuhoa1617@gmail.com>
2020-06-19 09:28:58 +0700

    update

---
+++ gihuy_lab456/ViewsModels/RegisterViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace gihuy_lab456.ViewsModels
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at:")]
+        [DataType(DataType.Password)]
+        [Display(Name ="Password")]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Display(Name ="Confirm password")]
+        [Compare("Password", ErrorMessage ="The password and: ")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Name { get; set; }
+    }
+}
